24840ecd00ef77955522199c9503cb92b0decf2e
Mahesh Sundaram <mahesh@omkara.dev>
2022-11-14 21:34:02 -1000

    Change default colors

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import * as three from "three";
 
 const scene = new three.Scene();
+scene.background = new three.Color(0xff9f9f);
 
 const camera = new three.PerspectiveCamera(
   75,
@@ -15,7 +16,7 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
 const geometry = new three.BoxGeometry(1, 1, 1);
-const material = new three.MeshBasicMaterial({ color: 0x00ff00 });
+const material = new three.MeshBasicMaterial({ color: 0xfffad7 });
 const cube = new three.Mesh(geometry, material);
 scene.add(cube);
 
